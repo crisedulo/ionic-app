35a6402d6cfc3a747834399f95bbee5cb293d91d
Cristian Lopez <crisedulo@gmail.com>
2020-05-26 11:57:58 -0600

    cambiando titulo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Drop</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Drop</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
